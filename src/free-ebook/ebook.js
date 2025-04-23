6623c216190f96a97bab30be486d0b25e57f925a
kingkongdevs <87683213+kingkongdevs@users.noreply.github.com>
2025-04-23 10:41:29 +1000

    rm /ebook

--- src/free-ebook/ebook.js
+++
@@ -1,9 +0,0 @@
-import { initModals } from '../assets/js/modal';
-import contentLoaded from '../assets/js/vendor/contentloaded';
-/**
- * Ebook page js here
- */
-
-contentLoaded(window, () => {
-    initModals();
-})
