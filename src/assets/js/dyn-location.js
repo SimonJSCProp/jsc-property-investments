6f192455282fdc8bbf19b1bf7096ea9b7839a0ad
kingkongdevs <robbie.c@kingkong.com.au>
2025-08-08 09:54:07 +1000

    Update dyn-location.js

--- src/assets/js/dyn-location.js
+++ src/assets/js/dyn-location.js
@@ -3,6 +3,10 @@ export default function dynLocation() {
 	const params = new URLSearchParams(window.location.search)
 	const location = params.get('utm_campaign')
 
+	// utm_campaign=kk_brand
+	// utm_campaign=kk_brisbane
+	// utm_campaign=kk_sydney
+
 	if (instances && location) {
 		instances.forEach((i) => {
 			i.textContent = location.replace('kk_', '')
